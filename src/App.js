17257fa9623c73c0735a3e4cfe012a37ce561464
stolani <olansiki@gmail.com>
2021-01-26 20:32:30 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
+
+
 
 class App extends Component {
     render() {
@@ -11,6 +14,7 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory />
             </div>
         );
     }
