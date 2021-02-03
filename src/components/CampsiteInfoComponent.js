b8de150bb4d37d516deb766a2bffcd65c4b49c02
stolani <olansiki@gmail.com>
2021-02-03 17:05:02 -0500

    Functional Components-2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,8 +7,8 @@ import {
   CardTitle,
 } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-  renderCampsite (campsite) {
+
+  function RenderCampsite ({campsite}) {
     return (
       <div className="col-md-5 m-1">
         <Card>
@@ -22,7 +22,7 @@ class CampsiteInfo extends Component {
     );
   }
 
-  renderComments (comments) {
+  function RenderComments ({comments}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -47,19 +47,21 @@ class CampsiteInfo extends Component {
     return <div />;
   }
 
-  render () {
-    if (this.props.campsite) {
-      return (
-        <div className="container">
-            <div className = "row">
-          {this.renderCampsite(this.props.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
-        </div>
-        </div>
-      );
+  function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
     }
     return <div />;
-  }
 }
 
 export default CampsiteInfo;
+
+
+  
