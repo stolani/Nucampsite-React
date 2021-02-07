ccef53cb8eeba91147b009ed611af7ab839e4ebd
stolani <olansiki@gmail.com>
2021-02-06 21:04:59 -0500

    About Us Page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import {COMMENTS} from '../shared/comments';
 import {PARTNERS} from '../shared/partners';
 import {PROMOTIONS} from '../shared/promotions';
+import About from './AboutComponent';
 
 class Main extends Component {
   constructor (props) {
@@ -44,19 +45,19 @@ class Main extends Component {
           />
       );
   };    
-  
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route
-            exact
-            path="/directory"
+            exact path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
           <Route exact path="/contactus" component={Contact} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+          <Route exact path="/aboutus" render={() => <About partners={this.state.partners} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
