c067fd1dc6ed3faeba77395dc7b363d04a5f4d79
stolani <olansiki@gmail.com>
2021-02-11 20:32:18 -0500

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,6 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import About from './AboutComponent';
